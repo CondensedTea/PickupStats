c3f01d037c3bcbf9baed1ca72a40a8a1b8d89f6b
Alex Tyshkevich <alex@tyshkevich.ru>
2021-11-05 18:31:18 +0300

    finished styling for dpm page

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,46 @@
 function createRatingList(items, type) {
-    const rating = document.createElement('ol');
+    const rating = document.createElement('tbody');
     document.getElementById('render').appendChild(rating);
 
-    items.forEach((item) => {
-        let li = document.createElement('li');
+    items.forEach((item, index) => {
+        let tr = rating.insertRow(-1);
+
+        let th = document.createElement('th');
+        th.setAttribute('scope', 'row')
+        let indexText = document.createTextNode(index + 1);
+        th.appendChild(indexText)
+        tr.appendChild(th)
+
+        let cellName = tr.insertCell(-1);
+        // let name = document.createTextNode(item.player_name);
+        let a = document.createElement('a')
+        a.text = (item?.player_name === "") ? item.steamid64 : item.player_name;
+        a.href = "https://steamcommunity.com/profiles/" + item.steamid64
+        cellName.appendChild(a)
+
         switch (type) {
             case 'kdr':
-                li.innerHTML = `${item.player_name} | kdr: ${item.kdr}, games: ${item.games}`;
-                break
+                //
+                //
+                // let cellKDR = tr.insertCell(0);
+                // cellName.innerHTML = item.kdr;
+                //
+                // // li.innerHTML = `${} | kdr: ${item.kdr}, games: ${item.games}`;
+                // break
             case 'dpm':
-                li.innerHTML = `${item.player_name} | dpm: ${item.dpm}, games: ${item.games}`;
+                let cellDPM = tr.insertCell(-1);
+                let dpm = document.createTextNode(item.dpm)
+                cellDPM.appendChild(dpm)
                 break
             case 'hpm':
-                li.innerHTML = `${item.player_name} | hpm: ${item.hpm}, games: ${item.games}`;
-                break
+                // li.innerHTML = `${item.player_name} | hpm: ${item.hpm}, games: ${item.games}`;
+                // break
         }
-        rating.appendChild(li);
+
+        let cellGamesCount = tr.insertCell(-1);
+        let games = document.createTextNode(item.games);
+        cellGamesCount.appendChild(games)
+        // rating.appendChild(li);
     });
 }
 
